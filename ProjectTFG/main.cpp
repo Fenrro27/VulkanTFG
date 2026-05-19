5b9b417ac87c5300e7b997ded2aadfc733eebebb
Fenrro27 <126926727+Fenrro27@users.noreply.github.com>
2026-05-20 00:02:34 +0200

    - fixed Bug

--- ProjectTFG/main.cpp
+++ ProjectTFG/main.cpp
@@ -1,29 +1,57 @@
-/**
- * @file main.cpp
- * @brief Archivo main.cpp
- */
-#include "GEApplication.h"
-#include <iostream>
-#include <stdexcept>
-
-//
-// PROYECTO: Project6
-//
-// DESCRIPCIÓN: Introduce el modelo de iluminación de Phong
-//
-int main()
-{
-	GEApplication app;
-
-	try
-	{
-		app.run();
-	}
-	catch (const std::exception& e)
-	{
-		std::cerr << e.what() << std::endl;
-		return EXIT_FAILURE;
-	}
-
-	return EXIT_SUCCESS;
+/**
+
+ * @file main.cpp
+
+ * @brief Archivo main.cpp
+
+ */
+
+#include "GEApplication.h"
+
+#include <iostream>
+
+#include <stdexcept>
+
+
+
+//
+
+// PROYECTO: Project6
+
+//
+
+// DESCRIPCIÓN: Introduce el modelo de iluminación de Phong
+
+//
+
+int main()
+
+{
+
+	GEApplication app;
+
+
+
+	try
+
+	{
+
+		app.run();
+
+	}
+
+	catch (const std::exception& e)
+
+	{
+
+		std::cerr << e.what() << std::endl;
+
+		return EXIT_FAILURE;
+
+	}
+
+
+
+	return EXIT_SUCCESS;
+
 }
